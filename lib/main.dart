a7c8ea465c28b13c9371bb2762c105ef908ecaca
emadnasri24 <emadnasri50@gmail.com>
2025-11-04 22:14:57 +0300

    init hive and open notes box

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter(); //first step
+  //second step open box that store data=colliction
+  await Hive.openBox(kNotesBox); //Hive is future should do await
   runApp(const NotesApp());
 }
 
